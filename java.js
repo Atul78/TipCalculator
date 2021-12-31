ce6adf87d3b28bf790cbd9b363508d35cb13a3d8
Atul78 <83898615+Atul78@users.noreply.github.com>
2021-12-31 15:35:18 +0530

    Update java.js

--- java.js
+++ java.js
@@ -1,20 +1,20 @@
 let calculate = document.querySelector('#calculate');
 
-calculate.addEventListener('click',()=>{
-    let totalbill = document.querySelector('#totalbill').value;
+calculate.addEventListener('click', ()=>{
+    let totalBill = document.querySelector('#totalBill').value;
     let tipPercent = document.querySelector('#tipPercent').value;
     let split = document.querySelector('#split').value;
 
-    //validate
-    if(totalbill === '' || tipPercent == 0 || split == 0){
-        alert('Please enter the values');
+    //validation
+
+    if(totalBill === '' || tipPercent == 0 || split == 0){
+        alert("Please Enter Values");
         return;
-    }
+    } 
 
-    let total = (totalbill * tipPercent) / split;
+    //calculate
+    let total = (totalBill * tipPercent) / split;
     total = total.toFixed(2);
-    // console.log(total);
     document.getElementById('tip').innerHTML = total;
-    
-
 })
+
